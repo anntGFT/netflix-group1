1e80ca2621afe862857b65f525997c053727c21e
anntGFT <109505145+anntGFT@users.noreply.github.com>
2022-08-26 14:30:24 +0200

    Update RecomendationController.java

--- src/main/java/com/sinensia/netflix/controllers/RecomendationController.java
+++ src/main/java/com/sinensia/netflix/controllers/RecomendationController.java
@@ -3,5 +3,13 @@ package com.sinensia.netflix.controllers;
 public class RecomendationController {
     
     
+    public List<Title> getBest(@RequestParam int limit) {
+List<Title> title;
+title = titleRepository.findAll();
+return title.stream()
+.sorted(Comparator.comparingDouble(Title::getUser_rating).reversed())
+.limit(limit) .collect(Collectors.toList());
+}
+    
 
 }
