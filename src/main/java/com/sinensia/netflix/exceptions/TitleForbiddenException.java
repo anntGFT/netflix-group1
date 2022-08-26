57cb6143798fa3060dcde50dfd93b027d75e94ce
IreneEscribano09 <ieee@gft.com>
2022-08-26 14:10:58 +0200

    Exceptions

---
+++ src/main/java/com/sinensia/netflix/exceptions/TitleForbiddenException.java
@@ -0,0 +1,9 @@
+package com.sinensia.netflix.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.FORBIDDEN, reason = "Forbidden request")
+public class TitleForbiddenException extends RuntimeException {
+    
+}
