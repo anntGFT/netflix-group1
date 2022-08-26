4225fffaf171df3ab7d09ffcc9908ebe01e5415f
GFT - Elías Gámez García <esgc@gft.com>
2022-08-26 14:14:20 +0200

    al final fkin controllers

---
+++ src/main/java/com/sinensia/netflix/controllers/DirectorController.java
@@ -0,0 +1,34 @@
+package com.sinensia.netflix.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sinensia.netflix.models.Director;
+import com.sinensia.netflix.repository.DirectorRepository;
+
+@RestController
+public class DirectorController {
+
+    @Autowired
+    DirectorRepository directorRepository;
+
+
+    @GetMapping("/director/name/{name}")
+    public List<Director> getCategoryByName(@PathVariable String name) {
+
+        List<Director> directorByName = directorRepository.findAll().parallelStream()
+
+            .filter(x -> x.getName().toLowerCase().contains(name.toLowerCase()))
+
+            .collect(Collectors.toList());
+
+        return directorByName;
+
+    }
+    
+}
